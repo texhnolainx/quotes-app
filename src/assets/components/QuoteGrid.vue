fb0499588aed8bb76a39e5646281e36a50bc8603
Texhno Lain <texhnolainx@gmail.com>
2019-12-18 21:09:13 +0100

    Added the initial files.

---
+++ src/assets/components/QuoteGrid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" :key="index" @click.native="deleteQuote(index)">{{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        components: {
+            'app-quote': Quote
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
+        }
+    }
+</script>
